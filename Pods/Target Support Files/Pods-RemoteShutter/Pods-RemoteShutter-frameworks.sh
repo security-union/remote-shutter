86b7ebaf66c87f4da0984b56a1e6cd310604e437
Dario Lencina <dario@armore.dev>
2020-09-23 23:06:16 +0000

    Resolve "Bug: Monitor timer is not working to adjust picture timer"

--- Pods/Target Support Files/Pods-RemoteShutter/Pods-RemoteShutter-frameworks.sh
+++ Pods/Target Support Files/Pods-RemoteShutter/Pods-RemoteShutter-frameworks.sh
@@ -197,12 +197,10 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/BFGallery/BFGallery.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Theater/Theater.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/BFGallery/BFGallery.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Starscream/Starscream.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Theater/Theater.framework"
 fi
