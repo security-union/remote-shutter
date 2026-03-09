db740222f1209426034a856cb51771400cc5826d
Dario Lencina <dario@securityunion.dev>
2026-03-08 21:40:54 -0400

    clear titles properly

--- RemoteCam/MonitorViewController.swift
+++ RemoteCam/MonitorViewController.swift
@@ -253,6 +253,9 @@ public class MonitorViewController: iAdViewController, UIImagePickerControllerDe
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
 
+        navigationItem.title = nil
+        navigationController?.navigationBar.prefersLargeTitles = false
+
         let appearance = UINavigationBarAppearance()
         appearance.configureWithTransparentBackground()
         navigationController?.navigationBar.standardAppearance = appearance
